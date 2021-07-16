8f164c240fe6bc9659a4c23591d281f3a313fba7
Tuhinadri Banerjee <tuhiniris@gmail.com>
2021-07-16 10:08:29 +0530

    Updated

--- Deliverables/16th July 2021 [Assignment 2]/Question 1/FClient.java
+++ Deliverables/16th July 2021 [Assignment 2]/Question 1/FClient.java
@@ -1,3 +1,33 @@
+/*
+
+General Protocol Requirements
+Client File Request Format
+
+REQUEST filename CRLF, with no spaces between REQUEST, filename and CRLF
+Server Message Format
+
+RDT sequence_number payload CRLF, where 
+the payload is a byte array of 512, and
+the sequence_number represents the consignment number and is an ascending number between 0 and 255 stored in 1 byte
+
+At the very last consignment, the message format is as follows:
+RDT sequence_number payload END CRLF
+In the last consignment, the payload is less than, or equals to 512 bytes.
+
+
+DONE-->
+* [Client Acknowledgement Format
+ACK sequence_number CRLF
+The sequence_number represents the consignment that the Client is expecting next, so it will be 1, 2, 3, 4, ... until the Client is notified of the last consignment.
+Upon receiving the last consignment, the Client sends an ACK with sequence_number 0, waits for 500ms and terminates the connection.
+Negative ACK is not required.
+
+Requirements for Stop-and-Wait
+Time-Out = 30ms]
+
+*/
+
+
 import java.net.*;
 import java.io.*;
 import java.util.*;
@@ -44,9 +74,8 @@ public class FClient extends Thread{
 			    System.out.println(reply);
 				fos.write(rp.getData());
 
-				if (reply.trim().equals("END")){ // if last consignment
-	
-					ack = "ACK_" + 0 + "_CRLF";
+				if (reply.trim().equals("END")){ // if last consignment	
+					ack = "ACK_" + 0 + "_CRLF + CLOSE TRANSMISSION";
 					sd=ack.getBytes();
 					sp=new DatagramPacket(sd,sd.length,InetAddress.getByName("127.0.0.1"),Integer.parseInt("10001"));
 					cs.send(sp);			
--- Deliverables/16th July 2021 [Assignment 2]/Question 1/FServer.java
+++ Deliverables/16th July 2021 [Assignment 2]/Question 1/FServer.java
@@ -1,3 +1,32 @@
+/*
+
+General Protocol Requirements
+Client File Request Format
+
+REQUEST filename CRLF, with no spaces between REQUEST, filename and CRLF
+Server Message Format
+
+RDT sequence_number payload CRLF, where 
+the payload is a byte array of 512, and
+the sequence_number represents the consignment number and is an ascending number between 0 and 255 stored in 1 byte
+
+At the very last consignment, the message format is as follows:
+RDT sequence_number payload END CRLF
+In the last consignment, the payload is less than, or equals to 512 bytes.
+
+
+DONE-->
+* [Client Acknowledgement Format
+ACK sequence_number CRLF
+The sequence_number represents the consignment that the Client is expecting next, so it will be 1, 2, 3, 4, ... until the Client is notified of the last consignment.
+Upon receiving the last consignment, the Client sends an ACK with sequence_number 0, waits for 500ms and terminates the connection.
+Negative ACK is not required.
+
+Requirements for Stop-and-Wait
+Time-Out = 30ms]
+
+*/
+
 import java.net.*;
 import java.io.*;
 import java.util.*;
