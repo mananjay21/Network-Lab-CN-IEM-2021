8b063271f168505aa59e45edd7ab387c85b9da0d
Tuhinadri Banerjee <tuhiniris@gmail.com>
2021-07-18 03:54:58 +0530

    Timed Out

--- Deliverables/16th July 2021 [Assignment 2]/Question 2/FServer.java
+++ Deliverables/16th July 2021 [Assignment 2]/Question 2/FServer.java
@@ -32,89 +32,90 @@ public class FServer {
 		byte[] rd, sd;
 
 		InetAddress ip;
-		int port;
-		
+		int port;		
 					
 			try {
-			//ss = new DatagramSocket(Integer.parseInt(args[0]));
-			ss = new DatagramSocket(10001);
-			System.out.println("Server is up....");
-			while(true){
-				ss.setSoTimeout(3000);
-			// read file into buffer
-			fis = new FileInputStream("demoText.html");
-			
-			int consignment=0;
-			String strConsignment;
-			int result = 0; // number of bytes read
-	 
-			while(true && result!=-1){
-				ss.setSoTimeout(3000); ////// Timeout on While True needs to be returned without stopping the code execution
-				rd=new byte[100];
-				sd=new byte[512];
-				 
-				rp = new DatagramPacket(rd,rd.length);
-				 
-				ss.receive(rp);
-				 
-				// get client's consignment request from DatagramPacket
-				ip = rp.getAddress(); 
-				port =rp.getPort();
-				System.out.println("Client IP Address = " + ip);
-				System.out.println("Client port = " + port);
-				System.out.println("REQUEST_"+"demoText.html"+"_CRLF");
+				//ss = new DatagramSocket(Integer.parseInt(args[0]));
+				ss = new DatagramSocket(10001);
+				System.out.println("Server is up....");
+				while(true){
+					ss.setSoTimeout(3000);
+					// read file into buffer
+					fis = new FileInputStream("demoText.html");			
+					int consignment=0;
+					String strConsignment;
+					int result = 0; // number of bytes read
+		 
+					while(true && result!=-1){
+						ss.setSoTimeout(3000); ////// Timeout on While True needs to be returned without stopping the code execution
+						rd=new byte[100];
+						sd=new byte[512];
+						 
+						rp = new DatagramPacket(rd,rd.length);						 
+						ss.receive(rp);
+						 
+						// get client's consignment request from DatagramPacket
+						ip = rp.getAddress(); 
+						port =rp.getPort();
+						
+						{
+						System.out.println("Client IP Address = " + ip);
+						System.out.println("Client port = " + port);
+						System.out.println("REQUEST_"+"demoText.html"+"_CRLF");
+						} // General Info Section
+						
+						strConsignment = new String(rp.getData());
+						System.out.println("CLIENT SENDS : "+(strConsignment));
+						String numberOnly= strConsignment.replaceAll("[^0-9]", ""); //EXTRA
+						
+						//consignment = Integer.parseInt(strConsignment.trim());
+						consignment = Integer.parseInt(numberOnly.trim());				
+						
+						System.out.println("Client ACK = " + consignment);
 
-				strConsignment = new String(rp.getData());
-				System.out.println("CLIENT SENDS : "+(strConsignment));
-				String numberOnly= strConsignment.replaceAll("[^0-9]", ""); //EXTRA
-				
-				//consignment = Integer.parseInt(strConsignment.trim());
-				consignment = Integer.parseInt(numberOnly.trim());				
-				
-				System.out.println("Client ACK = " + consignment);
-
-				// prepare data
-				result = fis.read(sd);
-				if (result == -1) {
-					//sd = new String("END").getBytes(); //RDT sequence_number payload END CRLF
-					sd = new String("RDT_"+consignment+"_512_"+"END_"+"CRLF").getBytes(); //RDT sequence_number payload END CRLF					
-					consignment = -1;
-				}
-				sp=new DatagramPacket(sd,sd.length,ip,port);
-				 
-				ss.send(sp);
-				 
-				rp=null;
-				sp = null;
-				 
-				System.out.println("Sent Consignment #" + consignment);
-	 
+						// prepare data
+						result = fis.read(sd);
+						
+						if (result == -1) {
+							//sd = new String("END").getBytes(); //RDT sequence_number payload END CRLF
+							sd = new String("RDT_"+consignment+"_512_"+"END_"+"CRLF").getBytes(); //RDT sequence_number payload END CRLF					
+							consignment = -1;
+						}
+						
+						sp=new DatagramPacket(sd,sd.length,ip,port);						 
+						ss.send(sp);
+						 
+						rp=null;
+						sp = null;
+						 
+						System.out.println("Sent Consignment #" + consignment);
+			 
+					}
 			}
-		}
 	}
 
-		catch (IOException ex) {
-			System.out.println(ex.getMessage());
-			return;
-		}
+			catch (IOException ex) {
+				System.out.println(ex.getMessage());
+				return;
+			}
 		
 				//catch (SocketTimeoutException ex) {		Skipping this will end the program when Request Times Out	
 	//		return;			I need the program to keep running on the event of a Timeout
 	//	}
 		
-		}
+		
 
 
-/*
-		finally {
-			try {
-				if (fis != null)
-					fis.close();
-			} catch (IOException ex) {
-				System.out.println(ex.getMessage());
-			}
-		} //End Finally
-*/		
+
+			finally {
+				try {
+					if (fis != null)
+						fis.close();
+				} catch (IOException ex) {
+					System.out.println(ex.getMessage());
+				}
+			} //End Finally
+	
 	} //End Main
-
+}
 
