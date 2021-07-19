773563a39a7ad11690e2e6ff8cdec3a1a3b20d83
Tuhinadri Banerjee <tuhiniris@gmail.com>
2021-07-19 12:54:55 +0530

    Upto Q3

---
+++ Deliverables/16th July 2021 [Assignment 2]/Question 4 - Lost Frame and Lost Acknowledgement/FClient.java
@@ -0,0 +1,109 @@
+/*
+
+Unfortunately, in the real-world, packets do get lost. 
+Consider the case where Frame 1 is lost in transit.  The Sender does not receive  the ACK within a specified time.
+Upon timeout, it remembers and re-sends Frame 1.
+
+Up to now, the FServer waits for the next request in the receive blocking call.  We need to implement a timeout on the DatagramSocket - see Lab #9, Exercise #2
+
+        DatagramSocket cs = new DatagramSocket();
+        cs.setSoTimeout(3000); // set timeout to 3000ms
+
+In the exception handling of SocketTimeoutException, the FServer needs to re-send the frame that is lost.
+
+(a) Write the pseudo-code for the Sender.
+
+(b) Implement the Stop-and-Wait ARQ with Lost Frame using RDT with Text File demoText.html.  Simulate the lost frames by having the FServer drop or not send them out (see PingServer from Lab #9, Exercise #2).  You may hard-code the frame(s) that will be lost, or use command-line parameters.  
+
+*/
+
+
+import java.net.*;
+import java.io.*;
+import java.util.*;
+
+
+ 
+public class FClient extends Thread{
+ 
+	public static void main(String[] args) {
+	    DatagramSocket cs = null;
+		FileOutputStream fos = null;
+
+		try {
+
+	    	cs = new DatagramSocket();
+	 
+			byte[] rd, sd;
+			String reply;
+			DatagramPacket sp,rp;
+			int count=0;
+			boolean end = false;
+			
+			System.out.println("REQUEST_"+"demoText.html"+"_CRLF");
+			fos = new FileOutputStream("demoText1.html");
+
+			while(!end)
+			{
+				try{
+			    //String ack = "" + count;
+			    String ack = "ACK_" + count + "_CRLF";
+			    	  
+				// send ACK      
+			    sd=ack.getBytes();	
+			    sp=new DatagramPacket(sd,sd.length,InetAddress.getByName("127.0.0.1"),Integer.parseInt("10001"));
+			    cs.send(sp);	
+
+				// get next consignment
+				rd=new byte[512];
+				rp=new DatagramPacket(rd,rd.length); 
+			    cs.receive(rp);	
+
+				// concat consignment 
+			    reply=new String(rp.getData());	 
+			    System.out.println(reply);
+				fos.write(rp.getData());
+
+				if ((reply.trim().equals("RDT_"+count+"_512_"+"END_"+"CRLF"))&&count<5){
+				//if ((reply.trim().equals("RDT_"+count+"_512_"+"END_"+"CRLF"))){ 			
+					ack = "ACK_" + 0 + "_CRLF + CLOSE TRANSMISSION";
+					sd=ack.getBytes();
+					sp=new DatagramPacket(sd,sd.length,InetAddress.getByName("127.0.0.1"),Integer.parseInt("10001"));
+					cs.send(sp);			
+					end = true;
+					try{Thread.sleep(500);}catch(InterruptedException e){System.out.println(e);}
+					System.out.println("Waited for 500MS");
+					System.exit(0);
+}
+				count++;
+			}
+
+			catch(SocketTimeoutException ex){
+				System.out.println("timeout");
+				return;
+			}
+
+			
+		}
+
+		} catch (IOException ex) {
+			System.out.println(ex.getMessage());
+
+		}
+		
+		 finally {
+
+			try {
+				if (fos != null)
+					fos.close();
+				if (cs != null)
+					cs.close();
+			} catch (IOException ex) {
+				System.out.println(ex.getMessage());
+			}
+		}
+		
+		
+		
+	}
+}
